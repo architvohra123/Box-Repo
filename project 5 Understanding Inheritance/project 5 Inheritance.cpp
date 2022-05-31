8a47e031072a98ce090eadf9d106c466d549309b
architvohra123 <archit0249.be21@chitkara.edu.in>
2022-05-31 18:36:51 +0530

    changed  pro 5

--- project 5 Understanding Inheritance/project 5 Inheritance.cpp
+++ project 5 Understanding Inheritance/project 5 Inheritance.cpp
@@ -4,6 +4,7 @@ class Vehicle{
 private:
     int types=2;
 public:
+    string color;
     string fuel="oil";
     void print(){
         cout<<"Vehicle is printed"<<endl;
@@ -19,7 +20,7 @@ protected:
 };
 class Car: public Vehicle{
 public:
-    string color;
+    int passengers;
     Car(){
         cout<<"Car Const"<<endl;
     }
@@ -30,6 +31,38 @@ public:
         cout<<"Car is printed";
     }
 };
+class Truck: public Vehicle{
+public:
+    int storage;
+    Truck(){
+        cout<<"Truck const"<<endl;
+    }
+    ~Truck(){
+        cout<<"Truck dest"<<endl;
+    }
+    void print(){
+        cout<<"Truck is printed"<<endl;
+    }
+};
+class Bus:public Car,public Truck{
+public:
+    Bus(){
+        cout<<"Bus const"<<endl;
+    }
+    ~Bus(){
+        cout<<"Bus dest"<<endl;
+    }
+    void print(){
+        cout<<"Bus is printed"<<endl;
+    }
+};
 int main(){
+    Vehicle v;
+    v.print();
     Car c;
+    c.print();
+    Truck t;
+    t.print();
+    Bus b;
+    b.print();
 }
